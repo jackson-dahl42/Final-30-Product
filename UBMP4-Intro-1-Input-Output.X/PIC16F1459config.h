f13fb3c462157619be429ce70215c81918039add
Jackson Dahl <99228002+jackson-dahl42@users.noreply.github.com>
2022-06-20 12:58:22 -0400

    Update PIC16F1459config.h

--- UBMP4-Intro-1-Input-Output.X/PIC16F1459config.h
+++ UBMP4-Intro-1-Input-Output.X/PIC16F1459config.h
@@ -18,17 +18,18 @@
 // These settings must exactly match those in the bootloader program if a 
 // bootloader will be used.
 
+// #pragma config statements should precede project file includes.
+// Use project enums instead of #define for ON and OFF.
 // CONFIG1
 #pragma config FOSC = INTOSC    // Oscillator Selection Bits (INTOSC oscillator: I/O function on CLKIN pin)
-#pragma config WDTE = SWDTEN    // Watchdog Timer Enable (WDT controlled by the SWDTEN bit in the WDTCON register)
+#pragma config WDTE = OFF       // Watchdog Timer Enable (WDT disabled)
 #pragma config PWRTE = ON       // Power-up Timer Enable (PWRT enabled)
-#pragma config MCLRE = OFF      // MCLR Pin Function Select (MCLR/VPP pin function is digital input)
+#pragma config MCLRE = ON       // MCLR Pin Function Select (MCLR/VPP pin function is MCLR)
 #pragma config CP = OFF         // Flash Program Memory Code Protection (Program memory code protection is disabled)
-#pragma config BOREN = SBODEN   // Brown-out Reset Enable (Brown-out Reset controlled by the SBOREN bit in the BORCON register)
+#pragma config BOREN = OFF      // Brown-out Reset Enable (Brown-out Reset disabled)
 #pragma config CLKOUTEN = OFF   // Clock Out Enable (CLKOUT function is disabled. I/O or oscillator function on the CLKOUT pin)
-#pragma config IESO = OFF       // Internal/External Switchover Mode (Internal/External Switchover Mode is disabled)
-#pragma config FCMEN = OFF      // Fail-Safe Clock Monitor Enable (Fail-Safe Clock Monitor is disabled)
-
+#pragma config IESO = ON        // Internal/External Switchover Mode (Internal/External Switchover Mode is enabled)
+#pragma config FCMEN = ON       // Fail-Safe Clock Monitor Enable (Fail-Safe Clock Monitor is enabled)
 // CONFIG2
 #pragma config WRT = OFF        // Flash Memory Self-Write Protection (Write protection off)
 #pragma config CPUDIV = NOCLKDIV// CPU System Clock Selection Bit (NO CPU system divide)
@@ -36,11 +37,16 @@
 #pragma config PLLMULT = 3x     // PLL Multipler Selection Bit (3x Output Frequency Selected)
 #pragma config PLLEN = ENABLED  // PLL Enable Bit (3x or 4x PLL Enabled)
 #pragma config STVREN = ON      // Stack Overflow/Underflow Reset Enable (Stack Overflow or Underflow will cause a Reset)
-#pragma config BORV = HI        // Brown-out Reset Voltage Selection (Brown-out Reset Voltage (Vbor), high trip point selected.)
+#pragma config BORV = LO        // Brown-out Reset Voltage Selection (Brown-out Reset Voltage (Vbor), low trip point selected.)
 #pragma config LPBOR = OFF      // Low-Power Brown Out Reset (Low-Power BOR is disabled)
 #pragma config LVP = OFF        // Low-Voltage Programming Enable (High-voltage on MCLR/VPP must be used for programming)
 
-// #pragma config statements should precede project file includes.
-// Use project enums instead of #define for ON and OFF.
+#define _XTAL_FREQ 4000000      // 4MHz for now
+#define SW1         PORTAbits.RA3   // S1/Reset pushbutton input
+#define SW2         PORTBbits.RB4   // Pushbutton SW2 input
+#define SW3         PORTBbits.RB5   // Pushbutton SW3 input
+#define SW4         PORTBbits.RB6   // Pushbutton SW4 input
+#define SW5         PORTBbits.RB7   // Pushbutton SW5 input
+#define BEEPER      LATAbits.LATA4  // Piezo beeper (LS1) output
 
-#include <xc.h>
+
